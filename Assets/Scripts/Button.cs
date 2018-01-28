646081b65402360138d0eae23e21826fc9d307c2
AjaxGb <ajaxgb@gmail.com>
2018-01-28 12:54:36 -0500

    Add puzzles, better lasers

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	public Animator pressedAnimation;
+
+	private int animKeyPressed;
+	private HashSet<Collider2D> allTouching = new HashSet<Collider2D>();
+
+	void Awake() {
+		animKeyPressed = Animator.StringToHash("Pressed");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		allTouching.Add(other);
+		CheckPressed();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		allTouching.Remove(other);
+		CheckPressed();
+	}
+
+	public void CheckPressed() {
+		pressedAnimation.SetBool(animKeyPressed, allTouching.Count > 0);
+	}
+}
