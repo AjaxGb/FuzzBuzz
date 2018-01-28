eea113acb4e3d23e199ddfaef17592fe5f30709e
AjaxGb <ajaxgb@gmail.com>
2018-01-28 11:58:49 -0500

    Add laser

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class KillZone : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		PossessableBase body = other.GetComponentInParent<PossessableBase>();
+		if (body is HamsterBody) {
+			GameOverManager.GameOver("ZAPPED!");
+		}
+	}
+}
---
+++ Assets/Scripts/Player/LaserBody.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBody : PossessableBase {
+
+	public float offTime = 3;
+
+	public Animator animator;
+
+	public bool isOff;
+	public float offEndTime;
+
+	private int animKeyDeactivated;
+
+	void Start() {
+		if (!animator) animator = GetComponent<Animator>();
+
+		animKeyDeactivated = Animator.StringToHash("Deactivated");
+	}
+
+	void Update() {
+		if (isOff && Time.time > offEndTime) {
+			isOff = false;
+			animator.SetBool(animKeyDeactivated, false);
+		}
+	}
+
+	protected override bool MindIsValid(PlayerMind mind) {
+		return !isOff;
+	}
+
+	protected override void OnPossessed() {
+		Possessor.ReturnToBody();
+		isOff = true;
+		animator.SetBool(animKeyDeactivated, true);
+		offEndTime = Time.time + offTime;
+	}
+
+	public override void PossessedUpdate() { }
+	protected override void OnUnpossessed() { }
+}
